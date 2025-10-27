da41ef7253d143c61facbb49263162ab095b562b
7snnm7 <7snnm7@gmail.com>
2025-10-27 20:06:08 +0300

    Update convert.js

--- api/convert.js
+++ api/convert.js
@@ -1,5 +1,5 @@
 function convert(q) {
-  if (!q) return 'Input should be in this state “!c OX OY OZ” or “!c NX NY NZ” check out my creator twitch.tv/hassannm7';
+  if (!q) return '/ban test2';
   
   const parts = q.trim().split(/\s+/).filter(Boolean);
   const isOver = parts[0].toLowerCase() === 'o';
