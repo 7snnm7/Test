05f60f3fec5e56a7e4d0a7fe75a7f012e2b403dd
7snnm7 <7snnm7@gmail.com>
2025-10-27 20:27:28 +0300

    Update convert.js

--- api/convert.js
+++ api/convert.js
@@ -1,6 +1,21 @@
 function convert(q) {
-  if (!q) return 'test';
-  
+  const mapping = {
+    q: "q"
+  };
+
+  const hasArabic = /[\u0600-\u06FF]/.test(q);
+
+  if (hasArabic) {
+    return "Input should be in English, so I’ll shoutout my creator, check out twitch.tv/HassanNM7 :D";
+  }
+
+  const converted = q
+    .split("")
+    .map((char) => mapping[char.toLowerCase()] || char)
+    .join("");
+
+  return converted;
 }
 
-convert(q);
+var result = convert(q);
+result;
