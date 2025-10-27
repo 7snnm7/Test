a0ca2b8978939779d513405416b46b3e3559ea31
7snnm7 <7snnm7@gmail.com>
2025-10-27 19:52:22 +0300

    Update convert.js

--- api/convert.js
+++ api/convert.js
@@ -1,8 +1,10 @@
-(function() {
-  try {
-    const message = 'Input should be in this state “!c OX OY OZ” or “!c NX NY NZ” check out my creator twitch.tv/hassannm7';
-    console.log(JSON.stringify(message));
-  } catch (e) {
-    console.log(JSON.stringify("Error: " + e.message));
-  }
-})();
+function convert(q) {
+  if (!q) return 'Input should be in this state “!c OX OY OZ” or “!c NX NY NZ” check out my creator twitch.tv/hassannm7';
+  
+  const parts = q.trim().split(/\s+/).filter(Boolean);
+  const isOver = parts[0].toLowerCase() === 'o';
+  const nums = parts.filter(x => !isNaN(x)).map(Number);
+
+}
+
+convert(q);
