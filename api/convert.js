fa098d69af7e70e29a631eea5adaf7e848daf6da
7snnm7 <7snnm7@gmail.com>
2025-10-27 15:03:21 +0300

    Update convert.js

--- api/convert.js
+++ api/convert.js
@@ -1,24 +1,5 @@
 function convert(q) {
-  const mapping = {
-    q: "ض", w: "ص", e: "ث", r: "ق", t: "ف", y: "غ", u: "ع", i: "ه", o: "خ", p: "ح",
-    "[": "ج", "]": "د", a: "ش", s: "س", d: "ي", f: "ب", g: "ل", h: "ا", j: "ت",
-    k: "ن", l: "م", ";": "ك", "'": "ط", z: "ئ", x: "ء", c: "ؤ", v: "ر", b: "لا",
-    n: "ى", m: "ة", ",": "و", ".": "ز", "/": "ظ"
-  };
-
-  const hasArabic = /[\u0600-\u06FF]/.test(q);
-
-  if (hasArabic) {
-    return "Input should be in English, so I’ll shoutout my creator, check out twitch.tv/HassanNM7 :D";
-  }
-
-  const converted = q
-    .split("")
-    .map((char) => mapping[char.toLowerCase()] || char)
-    .join("");
-
-  return converted;
+  return "cd";
 }
 
-var result = convert(q);
-result;
+console.log(convert(q));
