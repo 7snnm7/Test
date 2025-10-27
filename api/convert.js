b045641b3fcfb2a8639a730ba67a979c67789a05
7snnm7 <7snnm7@gmail.com>
2025-10-27 19:43:49 +0300

    Update convert.js

--- api/convert.js
+++ api/convert.js
@@ -1,22 +1,8 @@
-function convert(q) {
-  if (!q) return 'Input should be in this state “!c OX OY OZ” or “!c NX NY NZ” check out my creator twitch.tv/hassannm7';
-  
-  const parts = q.trim().split(/\s+/).filter(Boolean);
-  const isOver = parts[0].toLowerCase() === 'o';
-  const nums = parts.filter(x => !isNaN(x)).map(Number);
-
-  if (nums.length < 2 || nums.length > 3)
-    return 'Input should be in this state “!c OX OY OZ” or “!c o NX NY NZ” check out my creator twitch.tv/hassannm7';
-
-  if (isOver) {
-    const x = Math.round(nums[0] * 8);
-    const z = Math.round(nums[nums.length - 1] * 8);
-    return `Overworld Side Coordinates Are: (${x}, Y, ${z})`;
-  } else {
-    const x = Math.round(nums[0] / 8);
-    const z = Math.round(nums[nums.length - 1] / 8);
-    return `Nether Side Coordinates Are: (${x}, Y, ${z})`;
+(function() {
+  try {
+    const message = 'Input should be in this state “!c OX OY OZ” or “!c NX NY NZ” check out my creator twitch.tv/hassannm7';
+    console.log(JSON.stringify(message));
+  } catch (e) {
+    console.log(JSON.stringify("Error: " + e.message));
   }
-}
-
-convert(q);
+})();
