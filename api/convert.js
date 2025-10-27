c2b39f714ef9b734baa7e79f3827870bac2095f1
7snnm7 <7snnm7@gmail.com>
2025-10-27 12:40:59 +0300

    Update convert.js

--- api/convert.js
+++ api/convert.js
@@ -1,8 +1,1 @@
-function convert(q) {
-    let nums = q.trim().split(" ").map(Number);
-    if (nums.length < 2 || nums.some(isNaN)) return "Input must be numbers";
-    let x = Math.round(nums[0] / 8);
-    let z = Math.round(nums[nums.length - 1] / 8);
-    return `(${x}, Y, ${z})`;
-}
-convert(q);
+x
