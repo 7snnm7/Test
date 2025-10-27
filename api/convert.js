f1b4beaed751e2e5a2342d9ce1c90dfc2bb8cfd2
7snnm7 <7snnm7@gmail.com>
2025-10-27 19:59:36 +0300

    Update convert.js

--- api/convert.js
+++ api/convert.js
@@ -5,6 +5,18 @@ function convert(q) {
   const isOver = parts[0].toLowerCase() === 'o';
   const nums = parts.filter(x => !isNaN(x)).map(Number);
 
+  if (nums.length < 2 || nums.length > 3)
+    return 'Input should be in this state “!c OX OY OZ” or “!c o NX NY NZ” check out my creator twitch.tv/hassannm7';
+
+  if (isOver) {
+    const x = Math.round(nums[0] * 8);
+    const z = Math.round(nums[nums.length - 1] * 8);
+    return `Overworld Side Coordinates Are: (${x}, Y, ${z})`;
+  } else {
+    const x = Math.round(nums[0] / 8);
+    const z = Math.round(nums[nums.length - 1] / 8);
+    return `Nether Side Coordinates Are: (${x}, Y, ${z})`;
+  }
 }
 
 convert(q);
