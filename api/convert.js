e3a6e92fd540e3ab9bf64c13456bbcf7abb053c7
7snnm7 <7snnm7@gmail.com>
2025-11-01 20:18:18 +0300

    Update convert.js

--- api/convert.js
+++ api/convert.js
@@ -1,42 +1,22 @@
-var q = q || "";
-q = q.trim();
+var map = {
+  q: "ض", w: "ص", e: "ث", r: "ق", t: "ف", y: "غ", u: "ع", i: "ه", o: "خ", p: "ح",
+  "[": "ج", "]": "د", a: "ش", s: "س", d: "ي", f: "ب", g: "ل", h: "ا", j: "ت",
+  k: "ن", l: "م", ";": "ك", "'": "ط", z: "ئ", x: "ء", c: "ؤ", v: "ر", b: "لا",
+  n: "ى", m: "ة", ",": "و", ".": "ز", "/": "ظ"
+};
 
-// if no input or single word
-if (!q || q.split(/\s+/).length === 1) {
-  "check out my creator twitch.tv/HassanNM7";
-} else {
-  var enToAr = {
-    q: "ض", w: "ص", e: "ث", r: "ق", t: "ف", y: "غ", u: "ع", i: "ه", o: "خ", p: "ح",
-    "[": "ج", "]": "د", a: "ش", s: "س", d: "ي", f: "ب", g: "ل", h: "ا", j: "ت",
-    k: "ن", l: "م", ";": "ك", "'": "ط", z: "ئ", x: "ء", c: "ؤ", v: "ر", b: "لا",
-    n: "ى", m: "ة", ",": "و", ".": "ز", "/": "ظ"
-  };
+// Reverse map for Arabic → English
+var reverseMap = {};
+for (var key in map) reverseMap[map[key]] = key;
 
-  var arToEn = {};
-  for (var key in enToAr) {
-    var val = enToAr[key];
-    arToEn[val] = key;
-  }
+function convert(q) {
+  let engToAr = [...q].map(ch => map[ch.toLowerCase()] || ch).join("");
+  let arToEng = [...q].map(ch => reverseMap[ch] || ch).join("");
 
-  var hasArabic = /[\u0600-\u06FF]/.test(q);
-
-  if (hasArabic) {
-    // Arabic → English
-    var out = "";
-    for (var i = 0; i < q.length; i++) {
-      var two = q.slice(i, i + 2);
-      if (arToEn[two]) {
-        out += arToEn[two];
-        i++;
-      } else if (arToEn[q[i]]) {
-        out += arToEn[q[i]];
-      } else {
-        out += q[i];
-      }
-    }
-    out;
-  } else {
-    // English → Arabic
-    q.split("").map(c => enToAr[c.toLowerCase()] || c).join("");
-  }
+  // Decide which direction: detect Arabic letters
+  if (/[ء-ي]/.test(q)) return arToEng;
+  else return engToAr;
 }
+
+// Nightbot expects plain text output
+convert(q);
