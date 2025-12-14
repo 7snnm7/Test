e2bd7f262867af41f9bc268e60568dc5f3b1cb19
7snnm7 <7snnm7@gmail.com>
2025-12-14 21:54:05 +0300

    Update convert.js

--- api/convert.js
+++ api/convert.js
@@ -1,19 +1,15 @@
-function getTitle(q) {
+function formatTitle(q, t) {
   if (!q) {
     "Please provide a channel name.";
     return;
   }
 
-  var url = "https://decapi.me/twitch/title/" + encodeURIComponent(q);
-
-  var title = fetch(url).text();
-
-  if (!title || title.toLowerCase().includes("offline")) {
+  if (!t || t.toLowerCase().includes("offline")) {
     "The channel " + q + " is currently offline.";
     return;
   }
 
-  'The current title of ' + q + ' is "' + title + '"';
+  'The current title of ' + q + ' is "' + t + '"';
 }
 
-getTitle(q);
+formatTitle(q, t);
